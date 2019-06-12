b68ccb4b726826674ad0b9bbdbe02b634178c5c2
marcelestial <marcel@spaceside.com>
2019-06-11 17:23:51 -0700

    6/11/19 error during U1 launch

--- src/Item.java
+++ src/Item.java
@@ -6,4 +6,20 @@ public class Item {
         this.name = name;
         this.weight = weight;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 }
--- src/Rocket.java
+++ src/Rocket.java
@@ -1,8 +1,9 @@
 public class Rocket implements Spaceship {
-
-    public Rocket(){
-
-    }
+    private int currentWeight;
+    private int weight;
+    private int maxCargo;
+    private double totalWeight = currentWeight + weight;
+    private int cost;
 
     @Override
     public boolean launch() {
@@ -16,13 +17,51 @@ public class Rocket implements Spaceship {
 
     @Override
     public boolean canCarry(Item item) {
-        boolean carriable = true;
-        //implement canCarry method: does item exceed weight limit?
-        return carriable;
+        if (maxCargo >= (currentWeight + item.getWeight())) {
+
+            return true;
+        } else {
+            System.out.println("Can't carry item: " + item.getName() + "\tWeight: " + item.getWeight());
+            return false;
+        }
     }
 
     @Override
     public void carry(Item item) {
-        //update weight of rocket
+        System.out.println("Carrying item: " + item.getName() + "\tWeight: " + item.getWeight());
+        currentWeight += item.getWeight();
+    }
+
+    public double getMaxCargo() {
+        return maxCargo;
+    }
+
+    public void setMaxCargo(int maxCargo) {
+        this.maxCargo = maxCargo;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public double getTotalWeight() {
+        totalWeight = getCurrentWeight() + getWeight();
+        return totalWeight;
+    }
+
+    public int getCurrentWeight() {
+        return currentWeight;
+    }
+
+    public void setCurrentWeight(int currentWeight) {
+        this.currentWeight = currentWeight;
     }
 }
